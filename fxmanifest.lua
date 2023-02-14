980aca8bce2ce810562e04a39902451166f878b4
Axterlisoft <125221740+NixeronTheDeveloper@users.noreply.github.com>
2023-02-14 18:54:44 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+--[[----------------------------------
+]]------------------------------------
+fx_version 'adamant'
+game 'gta5'
+author 'NixiTheDeveloper'
+version '1.0'
+lua54 'yes'
+
+shared_scripts {
+	'@es_extended/imports.lua', -- Remove if you're using a version before ESX 1.3
+	'config.lua'
+}
+
+server_scripts {
+	'server.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
