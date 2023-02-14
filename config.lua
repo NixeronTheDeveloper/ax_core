4edcb65e872b5e17dffb02c4fe4f8986460ad552
Axterlisoft <125221740+NixeronTheDeveloper@users.noreply.github.com>
2023-02-14 18:54:31 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+-- Old = Trigger
+-- New = Legacy
+Config.ESXtype = 'Old' 
+Config.NotifyType = "esx" -- Options = t-notify, esx, mythic_notify, okokNotify
+Config.OxInventory = true -- When true, ox inventory related exports will work.
+
+}
